991e1c8bb666eb2db7641da156374fab474c2918
razvanrgh <razvanrgh@yahoo.com>
2019-12-07 13:43:27 +0200

    added robots and implemented iron

--- main.js
+++ main.js
@@ -2,11 +2,18 @@ document.addEventListener('DOMContentLoaded', function(){
 
 var mineButon = document.querySelector('#mineButton');
 var researchButon = document.querySelector('#researchButton');
-var paragraf = document.querySelector('#valoare');
+var rocksValue = document.querySelector('#rocksValue');
+var ironValue = document.querySelector('#ironValue')
 var elem = document.getElementById("myBar");
+var pickaxeValue = document.querySelector("#inventoryContainer p");
+var robotsValue = document.querySelectorAll("#inventoryContainer p")[1];
 var bigContainer = document.querySelector('#bigContainer');
 var researchContainer = document.querySelector('#researchContainer');
-paragraf.innerHTML = 0;
+var researchMenuButton = document.querySelector('#researchMenuContainer button');
+var pickaxeUpgradeButtons = document.querySelectorAll('#researchPickaxeContainer button');
+var robotsUpgradeButtons = document.querySelectorAll("#researchRobotContainer button");
+rocksValue.innerHTML = 0;
+ironValue.innerHTML = 0;
 
 
 /* function made for the progress bar of a button
@@ -24,10 +31,34 @@ mineButon.addEventListener('click', function(e){
                 //what happens if the progress bar is done
                 if (width >= 100) {
                     clearInterval(id);
-                    paragraf.innerHTML = parseInt(paragraf.innerHTML) + 1;
+                    rocksValue.innerHTML = parseInt(rocksValue.innerHTML) + 1;
+                    isIron = ironChance();
+                    console.log(pickaxeValue.innerHTML);
+                    //chance to mine iron at pickaxe lvl 1 is 10%
+                    if(pickaxeValue.innerHTML === "pickaxe: 1"){
+                        console.log('pickaxe lvl is 1');
+                        if(isIron === 10) {
+                            ironValue.innerHTML = parseInt(ironValue.innerHTML) + 1;
+                        }
+                    //chance to mine iron at pickaxe lvl 2 is 20%
+                    } else if(pickaxeValue.innerHTML === "pickaxe: 2"){
+                        console.log('pickaxe lvl is 2');
+                        if(isIron > 8){
+                            ironValue.innerHTML = parseInt(ironValue.innerHTML) + 1;
+                        }
+                    //chance to mine iron at pickaxe lvl 3 is 30%
+                    } else if (pickaxeValue.innerHTML === "pickaxe: 3"){
+                        console.log('pickaxe lvl is 3');
+                        if(isIron > 7) {
+                            ironValue.innerHTML = parseInt(ironValue.innerHTML) + 1;
+                        }
+                    } else{
+                        console.log("nope");
+                    }
                     elem.style.width = "0";
                     mineButon.removeAttribute("disabled");
                     console.log("done");
+                    console.log(isIron);
                 //what happens if the progress bar is not done
                 } else {
                 width++;
@@ -36,11 +67,58 @@ mineButon.addEventListener('click', function(e){
         }
 })
 
+//listener to go to the research tab
 researchButon.addEventListener('click', function(e){
     console.log(bigContainer);
     bigContainer.style.display = 'none';
-    researchContainer.style.display = 'block';
+    researchContainer.style.display = 'flex';
+    researchContainer.style.justifyContent = 'space-between';
 })
+
+//listener to go back to the menu
+researchMenuButton.addEventListener('click', function(e){
+    researchContainer.style.display = 'none';
+    bigContainer.style.display = 'flex';
+})
+
+
+function pickaxeUpgradeListener(index) {
+    console.log("pickaxe upgrade number " + (index + 1));
+    pickaxeValue.innerHTML = "pickaxe: " + (index + 1);
+    pickaxeUpgradeButtons[index].style.border = "2px solid rgba(255, 255, 255, 0.2)"
+    pickaxeUpgradeButtons[index].style.color = "rgba(255, 255, 255, 0.3)"
+    pickaxeUpgradeButtons[index].setAttribute('disabled', 'true');
+}
+
+function robotUpgradeListener(index) {
+    console.log("lil' robots upgrade number " + (index + 1));
+    robotsValue.innerHTML = "lil' robots: " + (index + 1);
+    robotsUpgradeButtons[index].style.border = "2px solid rgba(255, 255, 255, 0.2)"
+    robotsUpgradeButtons[index].style.color = "rgba(255, 255, 255, 0.3)"
+    robotsUpgradeButtons[index].setAttribute('disabled', 'true');
+};
+
+for(i=0; i < pickaxeUpgradeButtons.length; i++){
+    //Research bind method and closure inside loops!
+    //One way to do it is to store the item in a 'let' variable
+    //and use that to put the listener on. (errr, still doesn't work. Use bind)
+    //here's a link: https://stackoverflow.com/questions/750486/javascript-closure-inside-loops-simple-practical-example
+    pickaxeUpgradeButtons[i].addEventListener('click',
+     pickaxeUpgradeListener.bind(null,i));
+}
+
+for(i=0; i<robotsUpgradeButtons.length; i++) {
+    robotsUpgradeButtons[i].addEventListener('click',
+    robotUpgradeListener.bind(null, i));
+}
+
+
+
+function ironChance(){
+    var value = Math.floor(Math.random() * 10) + 1;
+    return value;
+}
+
 
 
 });
