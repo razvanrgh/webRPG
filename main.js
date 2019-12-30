3fb2f1098402661ab621e6f30487f46bf1baaaa3
razvanrgh <razvanrgh@yahoo.com>
2019-12-30 03:08:03 +0200

    implemented robots and scavenging button

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 document.addEventListener('DOMContentLoaded', function(){
 
 var mineButon = document.querySelector('#mineButton');
-var researchButon = document.querySelector('#researchButton');
+var researchButton = document.querySelector('#researchButton');
+var scavengeButton = document.querySelector('#scavengingButton');
+var scavengeContainer = document.querySelector('#scavengingContainer');
 var rocksValue = document.querySelector('#rocksValue');
 var ironValue = document.querySelector('#ironValue')
-var elem = document.getElementById("myBar");
+var elem = document.querySelectorAll(".myBar");
 var pickaxeValue = document.querySelector("#inventoryContainer p");
 var robotsValue = document.querySelectorAll("#inventoryContainer p")[1];
 var bigContainer = document.querySelector('#bigContainer');
@@ -12,8 +14,10 @@ var researchContainer = document.querySelector('#researchContainer');
 var researchMenuButton = document.querySelector('#researchMenuContainer button');
 var pickaxeUpgradeButtons = document.querySelectorAll('#researchPickaxeContainer button');
 var robotsUpgradeButtons = document.querySelectorAll("#researchRobotContainer button");
+var idRobot;
 rocksValue.innerHTML = 0;
 ironValue.innerHTML = 0;
+console.log(researchButton);
 
 
 /* function made for the progress bar of a button
@@ -23,14 +27,105 @@ mineButon.addEventListener('click', function(e){
     // can't push it again while it's loading
     mineButon.setAttribute("disabled", "true");
     console.log("click");
+    var width = 1;
+    //because the interval is set at 10ms, the width will increase
+    //1% every 10ms. => progress bar of 1 second
+    var id = setInterval(frame, 10);
+    function frame() {
+        //what happens if the progress bar is done
+        if (width >= 100) {
+            clearInterval(id);
+            console.log(elem[1]);
+            console.log(elem);
+            rocksValue.innerHTML = parseInt(rocksValue.innerHTML) + 1;
+            isIron = ironChance();
+            console.log(pickaxeValue.innerHTML);
+            //chance to mine iron at pickaxe lvl 1 is 10%
+            if(pickaxeValue.innerHTML === "pickaxe: 1"){
+                console.log('pickaxe lvl is 1');
+                if(isIron === 10) {
+                    ironValue.innerHTML = parseInt(ironValue.innerHTML) + 1;
+                }
+                //chance to mine iron at pickaxe lvl 2 is 20%
+            } else if(pickaxeValue.innerHTML === "pickaxe: 2"){
+                console.log('pickaxe lvl is 2');
+                if(isIron > 8){
+                    ironValue.innerHTML = parseInt(ironValue.innerHTML) + 1;
+                }
+                //chance to mine iron at pickaxe lvl 3 is 30%
+            } else if (pickaxeValue.innerHTML === "pickaxe: 3"){
+                console.log('pickaxe lvl is 3');
+                if(isIron > 7) {
+                    ironValue.innerHTML = parseInt(ironValue.innerHTML) + 1;
+                }
+            } else{
+                console.log("nope");
+            }
+            width = 0;
+            elem[1].style.width = width;
+            mineButon.removeAttribute("disabled");
+            console.log("done");
+            console.log(isIron);
+        //what happens if the progress bar is not done
+        } else {
+        width++;
+        console.log(width);
+        elem[1].style.width = width + "%";
+        }
+    }
+})
+
+//listener to go to the research tab
+researchButton.addEventListener('click', function(e){
+    console.log(bigContainer);
+    bigContainer.style.display = 'none';
+    researchContainer.style.display = 'flex';
+    researchContainer.style.justifyContent = 'space-between';
+})
+
+//listener to go back to the menu
+researchMenuButton.addEventListener('click', function(e){
+    researchContainer.style.display = 'none';
+    bigContainer.style.display = 'flex';
+})
+
+
+function pickaxeUpgradeListener(index) {
+    if(rocksValue.innerHTML > 9){
+        console.log("pickaxe upgrade number " + (index + 1));
+        pickaxeValue.innerHTML = "pickaxe: " + (index + 1);
+        pickaxeUpgradeButtons[index].style.border = "2px solid rgba(255, 255, 255, 0.2)"
+        pickaxeUpgradeButtons[index].style.color = "rgba(255, 255, 255, 0.3)"
+        pickaxeUpgradeButtons[index].setAttribute('disabled', 'true');
+        rocksValue.innerHTML = rocksValue.innerHTML - 10;
+    } else {
+        console.log('not enough minerals');
+        console.log(rocksValue.innerHTML);
+    }
+}
+
+function robotUpgradeListener(index) {
+    console.log("lil' robots upgrade number " + (index + 1));
+    robotsValue.innerHTML = "lil' robots: " + (index + 1);
+    robotsUpgradeButtons[index].style.border = "2px solid rgba(255, 255, 255, 0.2)"
+    robotsUpgradeButtons[index].style.color = "rgba(255, 255, 255, 0.3)"
+    robotsUpgradeButtons[index].setAttribute('disabled', 'true');
             var width = 1;
             //because the interval is set at 10ms, the width will increase
             //1% every 10ms. => progress bar of 1 second
-            var id = setInterval(frame, 10);
+            if (index == 0) {
+                idRobot = setInterval(frame, 100);
+            } else if (index == 1) {
+                idRobot = setInterval(frame, 75);
+            } else {
+                idRobot = setInterval(frame, 50);
+            }
             function frame() {
                 //what happens if the progress bar is done
                 if (width >= 100) {
-                    clearInterval(id);
+                    //clearInterval(id);
+                    console.log(elem[(index + 2)])
+                    console.log((index + 2));
                     rocksValue.innerHTML = parseInt(rocksValue.innerHTML) + 1;
                     isIron = ironChance();
                     console.log(pickaxeValue.innerHTML);
@@ -55,47 +150,16 @@ mineButon.addEventListener('click', function(e){
                     } else{
                         console.log("nope");
                     }
-                    elem.style.width = "0";
-                    mineButon.removeAttribute("disabled");
+                    width = 0;
+                    elem[(index + 2)].style.width = width;
                     console.log("done");
                     console.log(isIron);
                 //what happens if the progress bar is not done
                 } else {
                 width++;
-                elem.style.width = width + "%";
+                elem[(index + 2)].style.width = width + "%";
                 }
         }
-})
-
-//listener to go to the research tab
-researchButon.addEventListener('click', function(e){
-    console.log(bigContainer);
-    bigContainer.style.display = 'none';
-    researchContainer.style.display = 'flex';
-    researchContainer.style.justifyContent = 'space-between';
-})
-
-//listener to go back to the menu
-researchMenuButton.addEventListener('click', function(e){
-    researchContainer.style.display = 'none';
-    bigContainer.style.display = 'flex';
-})
-
-
-function pickaxeUpgradeListener(index) {
-    console.log("pickaxe upgrade number " + (index + 1));
-    pickaxeValue.innerHTML = "pickaxe: " + (index + 1);
-    pickaxeUpgradeButtons[index].style.border = "2px solid rgba(255, 255, 255, 0.2)"
-    pickaxeUpgradeButtons[index].style.color = "rgba(255, 255, 255, 0.3)"
-    pickaxeUpgradeButtons[index].setAttribute('disabled', 'true');
-}
-
-function robotUpgradeListener(index) {
-    console.log("lil' robots upgrade number " + (index + 1));
-    robotsValue.innerHTML = "lil' robots: " + (index + 1);
-    robotsUpgradeButtons[index].style.border = "2px solid rgba(255, 255, 255, 0.2)"
-    robotsUpgradeButtons[index].style.color = "rgba(255, 255, 255, 0.3)"
-    robotsUpgradeButtons[index].setAttribute('disabled', 'true');
 };
 
 for(i=0; i < pickaxeUpgradeButtons.length; i++){
@@ -111,6 +175,31 @@ for(i=0; i<robotsUpgradeButtons.length; i++) {
     robotsUpgradeButtons[i].addEventListener('click',
     robotUpgradeListener.bind(null, i));
 }
+
+scavengeButton.addEventListener('click', function(e){
+    console.log(scavengeContainer);
+    scavengeContainer.style.display = 'flex';
+    var width = 1;
+    //because the interval is set at 10ms, the width will increase
+    //1% every 10ms. => progress bar of 1 second
+    var id = setInterval(frame, 100);
+    function frame() {
+        //what happens if the progress bar is done
+        if (width >= 100) {
+            clearInterval(id);
+            console.log(elem[0]);
+            width = 0;
+            elem[0].style.width = width;
+            scavengeContainer.style.display = 'none';
+            console.log("done");
+        //what happens if the progress bar is not done
+        } else {
+        width++;
+        console.log(width);
+        elem[0].style.width = width + "%";
+        }
+    }
+});
 
 
 
